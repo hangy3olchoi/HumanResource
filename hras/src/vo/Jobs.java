83f5f7e24356d516724e03c6a590c5c51d96a9a4
Hangyeol Choi <92049334+hangy3olchoi@users.noreply.github.com>
2021-10-25 21:52:08 +0900

    Add files via upload

---
+++ hras/src/vo/Jobs.java
@@ -0,0 +1,46 @@
+package vo;
+
+public class Jobs {
+	private String job_id;
+	private String job_title;
+	private int min_salary;
+	private int max_salary;
+	
+	public String getJob_id() {
+		return job_id;
+	}
+	
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+	
+	public String getJob_title() {
+		return job_title;
+	}
+	
+	public void setJob_title(String job_title) {
+		this.job_title = job_title;
+	}
+	
+	public int getMin_salary() {
+		return min_salary;
+	}
+	
+	public void setMin_salary(int min_salary) {
+		this.min_salary = min_salary;
+	}
+	
+	public int getMax_salary() {
+		return max_salary;
+	}
+	
+	public void setMax_salary(int max_salary) {
+		this.max_salary = max_salary;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + job_id + " - " + job_title;
+	}
+	
+}
